af11e8f1053c2cc4200c070c1a54d20bfeba4369
Lam <vo2022@protonmail.com>
2023-05-22 17:23:45 -0700

    refactored to include styled components, react-router 6.11.12

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Navbar, Sidebar, Footer } from "./components";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
